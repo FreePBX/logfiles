caa2a574bb5b9f3d253a708f1fd04e665d3c00b4
Rob Thomas <xrobau@gmail.com>
2006-05-01 03:30:13 +0000

    Fix hardcoded webroot, ref #749

--- logfiles.php
+++ logfiles.php
@@ -1,4 +1,4 @@
-<a target="AsteriskFullLog" href="/admin/modules/logfiles/asterisk-full-log.php">Asterisk Full debug log (last 2000 lines)</a><br>
+<a target="AsteriskFullLog" href="modules/logfiles/asterisk-full-log.php">Asterisk Full debug log (last 2000 lines)</a><br>
 <br>
 <br>
 <br>
