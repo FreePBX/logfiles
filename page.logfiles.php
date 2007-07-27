ec54e3472c74411904496a77ebd6fbb16467989b
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-07-27 00:34:04 +0000

    #1442 remove access problem and put logfile in content div instead of new window

--- page.logfiles.php
+++ page.logfiles.php
@@ -1,15 +1,33 @@
 <?php
 
-$display=$_REQUEST['display'];
+$display = $_REQUEST['display'];
+$type = isset($_REQUEST['type']) ? $_REQUEST['type'] : 'tool';
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
 
-switch($display) {
+?>
+</div>
+<div class="content">
+<?php
+
+switch($action) {
+	case 'showlog':
+?>
+		<h2>
+			<?php echo sprintf(_('%s - last 2000 lines'),$amp_conf['ASTLOGDIR']."/full") ?>
+		</h2>
+		<a href="config.php?<?php echo "display=$display&type=$type&action=showlog"?>"><?php echo _("Redisplay Asterisk Full debug log (last 2000 lines)") ?></a><br>
+		<hr><br>
+		<?php
+		echo system ('tail --line=2000 '.$amp_conf['ASTLOGDIR'].'/full | sed -e "s/$/<br>/"'); 
+		break;
+
 	default:
-		
-		echo "<h2>Asterisk Log Files</h2>";
-        include 'logfiles.php';
-
+		echo "<h2>"._("Asterisk Log Files")."</h2>";
+?>
+				<a href="config.php?<?php echo "display=$display&type=$type&action=showlog"?>"><?php echo _("Display Asterisk Full debug log (last 2000 lines)") ?></a><br>
+				<br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
     break;
-	    
 }
 ?>
-
+</div>
