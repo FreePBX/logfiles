ea161570809ae3d9c284013c985ee2474fd78696
Andrew Nagy <andrew.nagy@the159.com>
2013-09-05 18:41:06 +0000

    FREEPBX-6548 #close Fix navigation highlighting issues

--- page.logfiles.php
+++ page.logfiles.php
@@ -1,75 +1,12 @@
 <?php
-if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
+require_once(dirname(__FILE__) .'/header.php');
+$var['files'] = logfiles_list();
 
-$getvars = array(
-	'action'		=> '',
-	'dateformat'	=> '',
-	'display' 		=> '',
-	'appendhostname'=> '',
-	'logfile'		=> '',
-	'logfiles'		=> '',
-	'lines'			=> '',
-	'queue_log'		=> '',
-	'rotatestrategy'=> '',
-	'view'			=> ''
-);
-
-foreach ($getvars as $k => $v) {
-	$var[$k]	= isset($_REQUEST[$k]) ? $_REQUEST[$k] : $v;
-}
-
-//sanitize input
-$var['lines'] = preg_replace("/[^0-9]/", "", $var['lines']);
-$var['logfile'] = preg_replace("/[^0-9]/", "", $var['logfile']);
-
-//echo logfiles_rnav();
-
-//respond to ajax requests
-if ($var['lines']) {
-	while (ob_get_level()) {
-		ob_end_clean();
+//find 'full' file
+foreach ($var['files'] as $k => $v) {
+	if($v == 'full') {
+		$var['full'] = $k;
+		break;
 	}
-	echo logfiles_get_logfile($var['lines'], $var['logfile']);
-	exit();
-	
 }
-
-switch ($var['action']) {
-	case 'save':
-		logfiles_put_opts($var);
-		break;
-}
-
-
-switch($var['view']) {
-	case 'opts':
-		$opts = logfiles_get_opts();
-		$opts['logfiles'][] = 	//always add a blank row, will work as default when nothing is set
-			$logfiles[] = array(
-								'name'		=> '', 
-								'debug'		=> '', 
-								'dtmf'		=> 'off', 
-								'error'		=> '', 
-								'fax'		=> 'off', 
-								'notice'	=> '', 
-								'verbose'	=> '', 
-								'warning'	=> ''
-					);
-		$var = array_merge($var, $opts);
-		//dbug('passing to view', $var);
-		echo load_view(dirname(__FILE__) . '/views/settings.php', $var);
-		break;
-	default:
-		$var['files'] = logfiles_list();
-		
-		//find 'full' file
-		foreach ($var['files'] as $k => $v) {
-			if($v == 'full') {
-				$var['full'] = $k;
-				break;
-			}
-		}
-		echo load_view(dirname(__FILE__) . '/views/logs.php', $var);
-    	break;
-}
-?>
+echo load_view(dirname(__FILE__) . '/views/logs.php', $var);
